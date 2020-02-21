d19855d4820c6d914c3a0a0ae9f306532658aa30
Eli Zevin <dizzyjaguar@gmail.com>
2020-02-20 17:37:27 -0800

    add initial seed data file

---
+++ data/seed-data.js
@@ -0,0 +1,29 @@
+module.exports = [
+    {
+        strain: 'O.G. Fire',
+        type: 'indica',
+        smell: 'gas',
+        thc: '23',
+        outdoor: true,
+        indoor: false,
+        imgUrl: 'someUrl'
+    },
+    {
+        strain: 'Blue Dream',
+        type: 'sativa',
+        smell: 'floral',
+        thc: '18',
+        outdoor: true,
+        indoor: false,
+        imgUrl: 'someUrl'
+    },
+    {
+        strain: 'Cookies',
+        type: 'hybrid',
+        smell: 'pepper',
+        thc: '5',
+        outdoor: false,
+        indoor: true,
+        imgUrl: 'someUrl'
+    },
+];
