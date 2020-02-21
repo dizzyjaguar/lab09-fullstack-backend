4325f589a522cea5931c8e84d3f4a372b15666eb
Eli Zevin <dizzyjaguar@gmail.com>
2020-02-21 10:13:34 -0800

    hello

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // load environment variables
-require('dotenv').config
+require('dotenv').config;
 
 // dependecies
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
-const pg = require('pg')
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const pg = require('pg');
 
 //database client
 const Client = pg.Client;
@@ -13,23 +13,61 @@ const client = new Client(process.env.DATABASE_URL);
 client.connect();
 
 //application setup
-const app = express()
-const PORT = process.env.PORT;
+const app = express();
+
 app.use(morgan('dev'));
 app.use(cors());
 
 //API ROUTES
 
-app.get('/hello', (req, res) => {
+app.get('/', (req, res) => {
     res.json({
         message: 'hello! :D'
     });   
 });
 
+app.get('/weed', async(req, res) => {
+    try {
+        const result = await client.query(`
+        SELECT 
+        strain,
+        type, 
+        smell,
+        thc,
+        outdoor,
+        indoor
+        FROM weed;
+        `);
+        console.log(result);
+        res.json(result.rows);
+
+    } catch (err) {
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const port = process.env.PORT || 3000;
-
 
 // start server
 app.listen(port, () => {
